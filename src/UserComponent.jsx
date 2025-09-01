cf590034968a27e050f9b690a6c91f17c70639a4
vboxuser <vboxuser@Sulduz.myguest.virtualbox.org>
2025-09-01 13:18:52 +0530

    added UserComponent file and modified json files

---
+++ src/UserComponent.jsx
@@ -0,0 +1,24 @@
+function Login(){
+    return(
+        <div>
+            <h1>User Login</h1>
+        </div>
+    )
+}
+
+export function Profile(){
+    return(
+        <div>
+            <h1>Hii my name is Mango</h1>
+        </div>
+    )
+}
+
+export function Settings(){
+    return(
+        <div>
+            <h1>My setting is with grapes</h1>
+        </div>
+    )
+}
+export default Login;
